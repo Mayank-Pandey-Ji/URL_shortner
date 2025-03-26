d031af7940ba252facf3102fc43491460b116884
Mayank-Pandey-Ji <pandeymayank1099@gmail.com>
2025-03-26 23:37:19 +0530

    cors sahi kiya hai

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,6 @@ app.use(
     cors({
       origin: "https://url-consiser.vercel.app", 
       credentials: true, 
-      methods: "GET,POST,PUT,DELETE,OPTIONS",
-      allowedHeaders: "Content-Type, Authorization"
     })
   );
 app.use(express.json());
